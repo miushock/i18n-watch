05f2fbb712bd4890ba9ec79bba01e0926bb7ce19
liu-kai-github <31062816+liu-kai-github@users.noreply.github.com>
2020-02-14 17:59:10 +0800

    完善结构

--- bin/i18n-watch.js
+++ bin/i18n-watch.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
-process.title = 'i18n-watch'
+process.title = 'i18n-watch';
 
-command = process.argv[2]
-args = process.argv.slice(3)
+command = process.argv[2];
+args = process.argv.slice(3);
 
-var Parser = require('../lib/parser.js')
+var Parser = require('../lib/parser.js');
 
-Parser.parse(command,args)
+Parser.parse(command, args);
